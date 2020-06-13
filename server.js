03a9d7ccca425188e0bf682c05a3c67722690569
Hyacinth Carter <hyacinthcarter50@gmail.com>
2020-06-13 10:52:38 +0100

    mongoClient options edit

--- server.js
+++ server.js
@@ -1,29 +1,29 @@
-const http = require("http");
-const mongoose = require("mongoose");
+const http = require('http');
+const mongoose = require('mongoose');
 const port = process.env.PORT || 3030;
-const app = require("./index.js");
+const app = require('./index.js');
 
-app.set("port", port);
+app.set('port', port);
 
 const server = http.createServer(app);
-const io = require("socket.io")(server);
+const io = require('socket.io')(server);
 
-let MONGO_URL = "mongodb://localhost:27017/";
+let MONGO_URL = 'mongodb://localhost:27017/';
 
-mongoose.connect(MONGO_URL, { useNewUrlParser: true });
+mongoose.connect(MONGO_URL, { useUnifiedTopology: true, useNewUrlParser: true });
 
 const db = mongoose.connection;
 
-db.on("error", () => console.log("error connecting to database"));
-db.once("open", () => console.log("connection established"));
+db.on('error', () => console.log('error connecting to database'));
+db.once('open', () => console.log('connection established'));
 
-io.on("connection", socket => {
-  console.log("client connected");
-  socket.on("newBooking", data => {
-    io.emit("newBooking", data);
+io.on('connection', socket => {
+  console.log('client connected');
+  socket.on('newBooking', data => {
+    io.emit('newBooking', data);
   });
-  socket.on("disconnect", () => {
-    console.log("client disconnected");
+  socket.on('disconnect', () => {
+    console.log('client disconnected');
   });
 });
 
