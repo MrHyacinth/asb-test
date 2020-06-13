8ac3989414dfea0327f6ea05e8c12c433c9ce948
Hyacinth Carter <hyacinthcarter50@gmail.com>
2020-06-13 04:40:46 +0100

    edit booking routes

--- routers/bookingRouter.js
+++ routers/bookingRouter.js
@@ -2,67 +2,19 @@ const express = require('express');
 const router = express.Router();
 const Bookings = require("../schemas/bookingModel");
 const Rooms = require("../schemas/roomModel");
+const BookingControllers = require('../controllers/bookingControllers');
 
-router.get('/admin', async (req, res) => {
-    try{
-        const bookings = await Bookings.find().exec();
-        res.status(200).json(bookings);
-    }catch(err){
-        res.status(500).json({ body: err.message });
-    }
-});
+router.get('/admin', BookingControllers.getBookings);
 
-router.get('/fetchAllBookings', async (req, res) => {
-    try{
-        const bookings = await Bookings.find().exec();
-        res.status(200).json(bookings);
-    }catch(err){
-        res.status(500).json({ body: err.message });
-    }
-});
+router.get('/fetchAllBookings', BookingControllers.getBookings);
 
-router.get('/fetchABooking/:id', async (req, res) => {
-    try{
-        const { id } = req.params;
-        const booking = await Bookings.findById(id).exec();
-        res.status(200).json(booking);
-    }catch(err){
-        res.status(500).json({ body: err.message });
-    }
-});
+router.get('/fetchABooking/:id', BookingControllers.getABooking);
 
-router.post("/addABooking", async (req, res) => {
-    try{
-        const  data  = req.body;
-        const newBooking = new Bookings(data);
-        const result = await newBooking.save();
-        res.status(201).json(result);
-        req.io.socket.emit('newBoking', data);
-    }catch(err){
-        res.status(500).json({ body: err.message });
-    }
-});
+router.post("/addABooking", BookingControllers.createBooking);
 
-router.put('/updateABooking/:id', async (req, res) => {
-    try{
-        const { id } = req.params;
-        const data  = req.body;
-        const result = await Bookings.findByIdAndUpdate(id, { $set: data }).exec();
-        res.status(200).json(result);
-    }catch(err){
-        res.status(500).json({ body: err.message});
-    }
-});
+router.put('/updateABooking/:id', BookingControllers.updateBooking);
 
-router.delete('/deleteABooking/:id', async (req, res) => {
-    try{
-        const { id } = req.params;
-        const result = await Bookings.deleteOne({ _id: id }).exec();
-        res.status(200).json(result);
-    }catch(err){
-        res.status(500).json({ body: err.message});
-    }
-});
+router.delete('/deleteABooking/:id', BookingControllers.deleteBooking);
 
 router.use((req, res) => {
     res.status(404).json({
